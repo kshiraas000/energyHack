308b6dcccc24c56295b6a8ec0be9b51237b9375d
Aashna <aashna.kshirsagar@gmail.com>
2025-01-18 21:16:26 -0500

    Update package versions for Python 3.11 compatibility

--- static/home.js
+++ static/home.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
+    const API_URL = 'https://smartsave-cgw1.onrender.com';
+
     $('#prediction-form').submit(function(e) {
         e.preventDefault();
         var formData = $(this).serialize();
 
         $.ajax({
-            url: '/predict',
+            url: `${API_URL}/predict`,  // Updated to use full URL
             type: 'POST',
             data: formData,
             success: function(response) {
-                $('#result').html('Predicted Price: $' + response.predicted_price.toFixed(2) + 
-                                  '<br>Recommended Action: ' + response.action);
+                $('#result').html('Predicted Price: €' + response.predicted_price.toFixed(2) + 
+                                '<br>Recommended Action: ' + response.action);
             },
             error: function(xhr, status, error) {
                 console.error(xhr.responseText);
-                $('#result').html('An error occurred: ' + xhr.responseText);
+                $('#result').html('An error occurred: ' + error);
             }
         });
     });
